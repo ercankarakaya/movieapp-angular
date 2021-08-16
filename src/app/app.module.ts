0cbb0ffe49244ccf281da274a26c907fb5683fe2
Ercan Karakaya <karakayaercan71@gmail.com>
2021-08-16 16:35:42 +0300

    Added Movies,Movie and Movie Details component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [  //componentler burada bildiriliyor.
     AppComponent,
     NavbarComponent,
-    CategoryComponent
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieDetailsComponent
   ],
   imports: [       // module eklenir.
     BrowserModule,
