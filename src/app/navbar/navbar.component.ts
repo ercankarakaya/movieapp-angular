4950eb2146d5579486b92c1a583b0bbc8c8bb935
Ercan Karakaya <karakayaercan71@gmail.com>
2021-08-19 14:57:54 +0300

    Project developed.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import localJsonData from '../LocalJsonData.json';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  
+  GeneralData=localJsonData;
+  //navTitle=localJsonData.TITLE.navTitle;
+
+
 
   constructor() { }
 
